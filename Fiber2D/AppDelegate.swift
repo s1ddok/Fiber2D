8d7a2ea539e30b8ca5b02b82cbd92bd2ce39f389
s1ddok <siddok@gmail.com>
2016-07-25 13:47:04 +0300

    Initial Commit

---
+++ Fiber2D/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Fiber2D
+//
+//  Created by Andrey Volodin on 25.07.16.
+//  Copyright © 2016 s1ddok. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
