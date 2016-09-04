2c63948acc5c3865204315969ab99b117b994afa
s1ddok <siddok@gmail.com>
2016-09-04 20:41:04 +0300

    ActionSpeed prototype

---
+++ Fiber2D/ActionSpeed.swift
@@ -0,0 +1,52 @@
+//
+//  ActionSpeed.swift
+//  Fiber2D
+//
+//  Created by Andrey Volodin on 04.09.16.
+//  Copyright © 2016 s1ddok. All rights reserved.
+//
+
+public struct ActionSpeedContainer: ActionContainer, Continous {
+    @inline(__always)
+    mutating public func update(state: Float) {
+        action.update(state: state)
+    }
+    
+    public mutating  func start(with target: AnyObject?) {
+        elapsed = 0
+        self.target = target
+        action.start(with: target)
+    }
+    
+    public mutating func stop() {
+        action.stop()
+    }
+    
+    public mutating func step(dt: Time) {
+        elapsed += dt * speed
+        
+        self.update(state: max(0, // needed for rewind. elapsed could be negative
+            min(1, elapsed / max(actionDuration,FLT_EPSILON)) // division by 0
+            )
+        )
+    }
+    
+    weak var target: AnyObject? = nil
+    public var tag: Int = 0
+    public let speed: Float
+    public let duration: Time
+    private let actionDuration: Time
+    private(set) public var elapsed:  Time = 0.0
+    
+    public var isDone: Bool {
+        return elapsed > duration
+    }
+    
+    private(set) var action: ActionContainer
+    public init(action: ActionContainer, speed: Float) {
+        self.action = action
+        self.actionDuration = (action as! FiniteTime).duration
+        self.duration = actionDuration / speed
+        self.speed = speed
+    }
+}
