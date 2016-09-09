bf77bb88235b2a7f67c41535367ac54ca8d575e3
s1ddok <siddok@gmail.com>
2016-09-09 16:20:26 +0300

    manual application lifecycle without @NSApplicationMain

--- Fiber2D/platform/macos/AppDelegate.swift
+++ Fiber2D/platform/macos/AppDelegate.swift
@@ -8,11 +8,9 @@
 
 import Cocoa
 
-@NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     var window: NSWindow!
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         CCSetup.createCustomSetup()
---
+++ Fiber2D/platform/macos/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  Fiber2D
+//
+//  Created by Andrey Volodin on 09.09.16.
+//  Copyright © 2016 s1ddok. All rights reserved.
+//
+
+import Cocoa
+import AppKit
+
+let delegate = AppDelegate()
+let app = NSApplication.shared()
+app.delegate = delegate
+app.run()
