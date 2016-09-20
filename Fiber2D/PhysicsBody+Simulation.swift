63b4086f927fe574b56bf24923aa77a393adc0ee
s1ddok <siddok@gmail.com>
2016-09-20 17:36:41 +0300

    body simulation implementation

--- Fiber2D/PhysicsBody+Simulation.swift
+++ Fiber2D/PhysicsBody+Simulation.swift
@@ -7,5 +7,41 @@
 //
 
 public extension PhysicsBody {
+    /**
+     * Applies a continuous force to body.
+     *
+     * @param force The force is applies to this body.
+     * @param offset A Vec2 object, it is the offset from the body's center of gravity in world coordinates.
+     */
+    public func apply(force: Vector2f, offset: Vector2f = Vector2f.zero) {
+        if isDynamic && mass != Float.infinity {
+            cpBodyApplyForceAtLocalPoint(chipmunkBody, cpVect(force), cpVect(offset))
+        }
+    }
     
+    /**
+     * reset all the force applied to body.
+     */
+    public func resetForces() {
+        cpBodySetForce(chipmunkBody, cpVect(vec2.zero))
+    }
+    
+    /**
+     * Applies a torque force to body.
+     *
+     * @param torque The torque is applies to this body.
+     */
+    public func apply(torque: Float) {
+        cpBodySetTorque(chipmunkBody, cpFloat(torque))
+    }
+    
+    /**
+     * Applies a immediate force to body.
+     *
+     * @param impulse The impulse is applies to this body.
+     * @param offset A Vec2 object, it is the offset from the body's center of gravity in world coordinates.
+     */
+    public func apply(impulse: Vector2f, offset: Vector2f = Vector2f.zero) {
+        cpBodyApplyImpulseAtLocalPoint(chipmunkBody, cpVect(impulse), cpVect(offset));
+    }
 }
