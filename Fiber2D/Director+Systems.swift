bdf0684ce17733452e881a9929fb9b40aee5e5c6
s1ddok <siddok@gmail.com>
2016-09-26 16:08:15 +0300

    system prototyping

---
+++ Fiber2D/Director+Systems.swift
@@ -0,0 +1,26 @@
+//
+//  Director+Systems.swift
+//  Fiber2D
+//
+//  Created by Andrey Volodin on 26.09.16.
+//  Copyright © 2016 s1ddok. All rights reserved.
+//
+
+public extension Director {
+    public func register(system: System) {
+        let idx = systems.first { s in s === system }
+        
+        guard idx == nil else {
+            return
+        }
+        
+        systems.append(system)
+        system.onAdd(to: self)
+    }
+    
+    public func remove(system: System) {
+        systems.removeObject(system)
+        system.onRemove()
+    }
+    
+}
