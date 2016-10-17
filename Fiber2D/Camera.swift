3544acf3c5731c6c9afc02481774228654f2d782
s1ddok <siddok@gmail.com>
2016-10-17 17:52:09 +0300

    break retain cycle, add few comments

--- Fiber2D/Camera.swift
+++ Fiber2D/Camera.swift
@@ -26,7 +26,7 @@ public class Camera: Node {
      */
     public let type: CameraType = .orthographic
     
-    public let viewport: ViewportNode
+    public unowned let viewport: ViewportNode
     
     init(viewport: ViewportNode) {
         self.viewport = viewport
@@ -47,10 +47,13 @@ public class Camera: Node {
         set {}
     }
     
+    // Override nodeToParentMatrix so input (like touches) in the viewport can be transformed into the node space of the content of the viewport.
     public override var nodeToParentMatrix: Matrix4x4f {
         let cs = viewport.contentSizeInPoints * 0.5
         let hw = cs.width
         let hh = cs.height
+        
+        // Scale and translate matrix to convert from clip coordiates to viewport internal coordinates
         let toProj = Matrix4x4f(vec4(hw,  0.0, 0.0, 0.0),
                                 vec4(0.0, hh,  0.0, 0.0),
                                 vec4(0.0, 0.0, 1.0, 0.0),
@@ -58,6 +61,7 @@ public class Camera: Node {
         return toProj * cameraMatrix
     }
     
+    // Camera matrix is used for drawing the contents of the viewport, relative to the camera.
     public var cameraMatrix: Matrix4x4f {
         let cameraTransform = super.nodeToParentMatrix.inversed
         return viewport.projection * cameraTransform
