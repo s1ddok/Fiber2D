a8f12072a2a7255e9ec4ff2d72b9dc49711efd36
s1ddok <siddok@gmail.com>
2016-11-15 00:25:10 +0300

    faster, cleaner and more precise transform calculation due to new trigonometry functions

--- Fiber2D/Node+Transform.swift
+++ Fiber2D/Node+Transform.swift
@@ -27,8 +27,8 @@ public extension Node {
         }
         
         // Get x and y
-        var x = Float(positionInPoints.x)
-        var y = Float(positionInPoints.y)
+        var x = positionInPoints.x
+        var y = positionInPoints.y
         // Rotation values
         // Change rotation code to handle X and Y
         // If we skew with the exact same value for both x and y then we're simply just rotating
@@ -36,15 +36,12 @@ public extension Node {
         var sx: Float = 0
         var cy: Float = 1
         var sy: Float = 0
-        if rotationalSkewX != 0° || rotationalSkewY != 0° {
-            let radiansX = -rotationalSkewX.radians
-            let radiansY = -rotationalSkewY.radians
-            cx = cosf(radiansX)
-            sx = sinf(radiansX)
-            cy = cosf(radiansY)
-            sy = sinf(radiansY)
+        if rotationalSkewX != .zero || rotationalSkewY != .zero {
+            sincos(-rotationalSkewX, &sx, &cx)
+            sincos(-rotationalSkewY, &sy, &cy)
         }
-        let needsSkewMatrix = skewX != 0° || skewY != 0°
+        
+        let needsSkewMatrix = skewX != .zero || skewY != .zero
         var scaleFactor: Float = 1
         if scaleType == .scaled {
             scaleFactor = Setup.shared.UIScale
@@ -65,8 +62,8 @@ public extension Node {
         // XXX: Try to inline skew
         // If skew is needed, apply skew and then anchor point
         if needsSkewMatrix {
-            let skewMatrix = Matrix4x4f(vec4(1.0, tanf(skewY.radians), 0.0, 0.0),
-                                        vec4(tanf(skewX.radians), 1.0, 0.0, 0.0),
+            let skewMatrix = Matrix4x4f(vec4(1.0, tan(skewY), 0.0, 0.0),
+                                        vec4(tan(skewX), 1.0, 0.0, 0.0),
                                         vec4(0.0, 0.0, 1.0, 0.0),
                                         vec4(0.0, 0.0, 0.0, 1.0))
             self.transform = transform * skewMatrix
