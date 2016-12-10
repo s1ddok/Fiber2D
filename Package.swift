5a59f912d1f14d5e8252055e6b80c8e06531becf
s1ddok <siddok@gmail.com>
2016-12-10 17:56:49 +0300

    remove swiftmath dependency, use the one from SwiftBGFX instead to prevent symbol duplication

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "Fiber2D",
     dependencies: [
-        .Package(url: "https://github.com/SwiftGFX/SwiftMath", Version(2, 2, 0)),
         .Package(url: "https://github.com/s1ddok/CChipmunk2D", Version(1, 0, 0)),
         .Package(url: "https://github.com/s1ddok/Cpng", Version(1, 0, 0))
     ],
