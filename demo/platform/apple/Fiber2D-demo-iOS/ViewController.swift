1fc0b9c1e1e7232857133d4e95efe06542d20d08
s1ddok <siddok@gmail.com>
2016-12-30 02:28:51 +0300

    add ios demo template

---
+++ demo/platform/apple/Fiber2D-demo-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fiber2D-demo-iOS
+//
+//  Created by Andrey Volodin on 30.12.16.
+//  Copyright © 2016 s1ddok. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
