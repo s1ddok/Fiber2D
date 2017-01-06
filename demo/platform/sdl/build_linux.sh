2a09790bb7d3306f4969cbd79b69dfad351f1166
s1ddok <siddok@gmail.com>
2017-01-07 00:43:36 +0100

    fix sdl build script to a new build model

--- demo/platform/sdl/build_linux.sh
+++ demo/platform/sdl/build_linux.sh
@@ -2,15 +2,11 @@ swift build \
 -Xswiftc -I../../../.build/debug \
 -Xswiftc -I../../../Packages/Cpng-1.0.0/Cpng/include \
 -Xswiftc -I../../../Packages/CChipmunk2D-1.0.0/CChipmunk2D/include \
--Xswiftc -I../../../external/SwiftBGFX/.build/debug \
 -Xlinker -L../../../.build/debug \
--Xlinker -L../../../external/SwiftBGFX/.build/debug \
 -Xlinker -lSwiftMath -Xlinker -lSwiftBGFX -Xlinker -lFiber2D -Xlinker -lGL -Xlinker -lX11
 
 cp ../../../.build/debug/libFiber2D.so .build/debug/libFiber2D.so
 cp ../../../.build/debug/libCpng.so .build/debug/libCpng.so
 cp ../../../.build/debug/libCChipmunk2D.so .build/debug/libCChipmunk2D.so
-cp ../../../external/SwiftBGFX/.build/debug/libCbgfx.so .build/debug/libCbgfx.so
-cp ../../../external/SwiftBGFX/.build/debug/libSwiftBGFX.so .build/debug/libSwiftBGFX.so
-cp ../../../external/SwiftBGFX/3rdparty/bgfx/.build/linux64_gcc/bin/libbgfxDebug.a \
-.build/debug/libbgfxDebug.a
+cp ../../../.build/debug/libCbgfx.so .build/debug/libCbgfx.so
+cp ../../../.build/debug/libSwiftBGFX.so .build/debug/libSwiftBGFX.so
