9f98ad57db18d070b9cf1e12fcab987568dbd92e
s1ddok <siddok@gmail.com>
2017-01-09 12:50:02 +0100

    rollback apple xcodeproj to  1fc0b9c and fix macOS demo

--- Fiber2D/ResponderManager+Mouse.swift
+++ Fiber2D/ResponderManager+Mouse.swift
@@ -7,9 +7,11 @@
 //
 
 #if os(macOS) || os(Linux)
+import SwiftMath
+
 public extension ResponderManager {
     
-    public func executeOnEachResponder(_ block: (Responder) -> Void, screenPosition: Point) {
+    internal func executeOnEachResponder(_ block: (Responder) -> Void, screenPosition: Point) {
         Director.pushCurrentDirector(director)
         // scan through responders, and find first one
         for responder in responderList.lazy.reversed() {
@@ -29,7 +31,7 @@ public extension ResponderManager {
     }
     
     // finds a responder object for an event
-    fileprivate func responder(for button: MouseButton) -> RunningResponder? {
+    internal func responder(for button: MouseButton) -> RunningResponder? {
         for touchEntry in runningResponderList {
             if touchEntry.button == button {
                 return touchEntry
@@ -39,7 +41,7 @@ public extension ResponderManager {
     }
     
     // adds a responder object ( running responder ) to the responder object list
-    fileprivate func add(responder: Responder, withButton button: MouseButton) {
+    internal func add(responder: Responder, withButton button: MouseButton) {
         // create a new input object
         let touchEntry = RunningResponder(target: responder)
         touchEntry.button = button
