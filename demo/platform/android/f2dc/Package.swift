73470b9f290045533d6044dbef226b0859de89c0
Andrey Volodin <siddok@gmail.com>
2017-01-25 01:42:16 +0300

    Build android demo in one step

--- demo/platform/android/f2dc/Package.swift
+++ demo/platform/android/f2dc/Package.swift
@@ -4,7 +4,8 @@ let package = Package(
     name: "f2dc",
     dependencies: [
         .Package(url: "https://github.com/s1ddok/CSDL2", majorVersion: 1),
-        .Package(url: "https://github.com/s1ddok/CAndroidAppGlue", majorVersion: 1)
+        .Package(url: "https://github.com/s1ddok/CAndroidAppGlue", majorVersion: 1),
+        .Package(url: "../../../../", Version(0, 0, 1))
     ]
 )
 
