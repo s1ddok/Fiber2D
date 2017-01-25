61440299f9452a69b4d4ec0c14dd8d7550d4a4cf
s1ddok <siddok@gmail.com>
2017-01-25 15:37:31 +0300

    conceptual Package.swift for multiple OSs

--- demo/platform/sdl/Package.swift
+++ demo/platform/sdl/Package.swift
@@ -1,8 +1,30 @@
 import PackageDescription
 
+var dependencies: [Package.Dependency] = [
+    .Package(url: "https://github.com/s1ddok/CSDL2", Version(1, 0, 0)),
+    .Package(url: "../../../", Version(0, 0, 1))
+]
+
+#if os(Android)
+dependencies += [
+    .Package(url: "https://github.com/s1ddok/CAndroidAppGlue", majorVersion: 1),
+]
+#endif
+
+#if os(Android)
+let exclude: [String] = ["Sources/main.swift"]
+#else
+let exclude: [String] = []
+#endif
+
+#if os(Android)
+let name = "f2dc"
+#else
+let name = "Fiber2D-demo"
+#endif
+
 let package = Package(
-    name: "Fiber2D-demo",
-    dependencies: [
-        .Package(url: "https://github.com/s1ddok/CSDL2", Version(1, 0, 0))
-    ]
+    name: name,
+    dependencies: dependencies,
+    exclude: exclude
 )
